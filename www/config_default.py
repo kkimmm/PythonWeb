cc343ca7ecd7a847edc9946b49272ed9ea07bd75
にゃんこ せんせい <1187130615@qq.com>
2019-04-12 18:11:14 +0800

    init project

---
+++ www/config_default.py
@@ -0,0 +1,17 @@
+#!/Users/gkai/anaconda3/bin/python
+#coding:utf-8
+
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'www-data',
+        'password': 'www-data',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
---
+++ www/test.py
@@ -0,0 +1,15 @@
+#!/Users/gkai/anaconda3/bin/python
+#coding:utf-8
+
+import orm
+import asyncio
+from models import User, Blog, Comment
+
+async def test(loop):
+    await orm.create_pool(loop=loop, user='root', password='password', db='awesome')
+    u = Blog(user_id='0015472266357569e74005330744ceca1d1f6fdd6b034a4000', user_name='T@qq.com', user_image='about:blank',name='Test', summary='Test', content='1234567890')
+    await u.save()
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test(loop))
+    # loop.close()
